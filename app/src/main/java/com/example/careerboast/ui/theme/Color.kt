4d313b4360eae7cc3fbfb5eb30c2daa52853a3aa
Rashid <ev4esem@gmail.com>
2024-03-14 10:58:32 +0300

    Create project, set up firebase Add res

---
+++ app/src/main/java/com/example/careerboast/ui/theme/Color.kt
@@ -0,0 +1,20 @@
+package com.example.careerboast.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+val GhostlyWhite = Color(0xFFF7FAFC)
+val White = Color(0xFFFFFFFF)
+val DarkBlue = Color(0xFF4F7396)
+val Blue = Color(0xFF1A5CE5)
+val Black = Color(0xFF000000)
+val LavenderElement = Color(0xFFE8EDF2)
+val LavenderText = Color(0xFFE8EBF2)
+val LavenderElementLight = Color(0xFFE8EBF4)
+val GrayLight = Color(0xFFF8F8F8)
+val Gainsborough = Color(0xFFD1DBE8)
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
